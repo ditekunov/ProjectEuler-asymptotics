1754dd9fd46c582663b11c10b6202959b281ca05
Tekunov Daniil <ditekunov@edu.hse.ru>
2018-05-09 22:04:21 +0300

    Added improved algo

--- haskell/Task1.hs
+++ haskell/Task1.hs
@@ -1,6 +1,19 @@
+--ghc 7.10
+
+
 basicAlgo :: Int -> Int
+improvedAlgo :: Int -> Int
 
+-- Basic algorithm with O(N) speed
 basicAlgo til = 
-    sum (filter(\x -> x `mod` 3 == 0 || x `mod` 5 == 0)[1..til])
+    sum (filter(\x -> x `mod` 3 == 0 || x `mod` 5 == 0)[1..til]) - til
 
-main = print $  basicAlgo(999)
+-- Improved algorithm with O(1) speed
+improvedAlgo til = 
+    ((3*((til-1) `div` 3)*(((til-1) `div` 3)+1)) `div` 2) + ((5*((til-1) `div` 5)*(((til-1) `div` 5)+1)) `div` 2) - ((15*((til-1) `div` 15)*(((til-1) `div` 15)+1)) `div` 2)
+    
+
+
+main = 
+    print $ [basicAlgo(1000), improvedAlgo(1000)]
+    
