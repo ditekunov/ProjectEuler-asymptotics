99db85931a0a1049eb78d258195b7882dee582ce
Tekunov Daniil <ditekunov@edu.hse.ru>
2018-05-13 18:07:02 +0300

    Added improved solution

--- haskell/Task3.hs
+++ haskell/Task3.hs
@@ -5,4 +5,19 @@ factorization til = (filter(\x -> til `mod` x == 0 && isPrime x)[1..til])
 isPrime :: Int -> Bool
 isPrime til = null (filter(\x -> til `mod` x == 0)[2..til-1])
 
+-- Improved O(n*log(log(n))) solution
+primesTo m = eratos [2..m] where
+   eratos (p : xs) 
+      | p*p > m   = p : xs
+      | otherwise = p : eratos (xs `minus` [p*p, p*p+p..m])
+                                  
+ 
+minus a@(x:xs) b@(y:ys) = case compare x y of
+         LT -> x : minus  xs b
+         EQ ->     minus  xs ys
+         GT ->     minus  a  ys
+minus a        b        = a 
+
+
+
 main = print $ (maximum (factorization 600851475143))
