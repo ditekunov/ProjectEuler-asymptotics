e69daeaf86bb9607b357d31cc878135b98815451
Tekunov Daniil <ditekunov@edu.hse.ru>
2018-05-15 16:32:45 +0300

    Update Task4.hs

--- haskell/Task4.hs
+++ haskell/Task4.hs
@@ -1,6 +1,6 @@
 --ghc 7.10
 
--- Basic algorithm with O(N) speed
+-- Basic algorithm with O(N^2) speed
 palindrome =
   maximum [x | y<-[100..999], z<-[y..999], let x=y*z, let s=show x, s==reverse s]
   
