d85c16efe083e08cddf06036b03fe1c570e6fcb1
Tekunov Daniil <ditekunov@yandex.ru>
2018-05-26 15:07:19 +0300

    Update Problem2.scala

--- scala/Problem2.scala
+++ scala/Problem2.scala
@@ -2,19 +2,30 @@ import scala.annotation.tailrec
 
 object Main extends App {
 
-  /**
-    * Basic algorithm with O(n) speed
-    */
-  def basicAlgo(until: Int = 4000000) = {
-    @tailrec
-    def fibonacciRecursion(listOfFibs: List[Int] = List(), first: Int = 1, second: Int = 1): List[Int] = {
-      if (second >= until) listOfFibs
-      else if (second % 2 == 0) fibonacciRecursion(listOfFibs :+ second, second, first + second)
-      else fibonacciRecursion(listOfFibs, second, first + second)
-    }
-    fibonacciRecursion()
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0)*0.000000001 + "s")
+    result
   }
 
-  println(basicAlgo().sum)
+  time {
+    /**
+      * Basic algorithm with O(n) speed
+      */
+    def basicAlgo(until: Int = 4000000) = {
+      @tailrec
+      def fibonacciRecursion(listOfFibs: List[Int] = List(), first: Int = 1, second: Int = 1): List[Int] = {
+        if (second >= until) listOfFibs
+        else if (second % 2 == 0) fibonacciRecursion(listOfFibs :+ second, second, first + second)
+        else fibonacciRecursion(listOfFibs, second, first + second)
+      }
+      fibonacciRecursion()
+    }
+
+    println(basicAlgo().sum)
+  }
+
 
 }
