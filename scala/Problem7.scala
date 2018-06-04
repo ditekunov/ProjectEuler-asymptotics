fa5b3195010b18d2ab08c3df5ddfe3fc2e29f428
Tekunov Daniil <ditekunov@yandex.ru>
2018-06-04 17:12:35 +0300

    Update Problem7.scala

--- scala/Problem7.scala
+++ scala/Problem7.scala
@@ -1,4 +1,9 @@
-lazy val ps: Stream[Int] = 2 #:: Stream.from(3).filter(i =>
-  ps.takeWhile(j => j * j <= i).forall(i % _ > 0))
+object Main extends App {
 
-val r = ps(10000)
+  //O(n*loglogn) 
+  lazy val ps: Stream[Int] = 2 #:: Stream.from(3).filter(i =>
+    ps.takeWhile(j => j * j <= i).forall(i % _ > 0))
+
+  val r = ps(10000)
+
+}
